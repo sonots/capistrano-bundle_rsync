5132bd14851867050caf97c4b706029098f64abb
Takumasa Ochi <aeroastro007@gmail.com>
2023-08-06 16:45:21 +0900

    Emulate `cap` command and `Capfile` in test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,20 @@
-require 'capistrano/all'
-
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'capistrano/bundle_rsync'
+
+# Emulate cap command
+# https://github.com/capistrano/capistrano/blob/31e142d56f8d894f28404fb225dcdbe7539bda18/bin/cap
+require "capistrano/all"
+
+# Emulate Capfile
+
+# Load DSL and Setup Up Stages
+require 'capistrano/setup'
+
+# Includes default deployment tasks
+require 'capistrano/deploy'
+
+if Gem::Version.new(Capistrano::VERSION) >= Gem::Version.new('3.7')
+  require 'capistrano/bundle_rsync/plugin'
+  install_plugin Capistrano::BundleRsync::Plugin
+else
+  require 'capistrano/bundle_rsync'
+end
