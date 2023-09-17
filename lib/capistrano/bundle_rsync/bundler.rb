519744d26bf54c2fcfe7277346e48ab5cad13c53
Takumasa Ochi <aeroastro007@gmail.com>
2023-09-17 14:00:27 +0900

    Add bundle_rsync_app_path to specify where to run bundle

--- lib/capistrano/bundle_rsync/bundler.rb
+++ lib/capistrano/bundle_rsync/bundler.rb
@@ -3,7 +3,7 @@ require 'capistrano/configuration/filter'
 
 class Capistrano::BundleRsync::Bundler < Capistrano::BundleRsync::Base
   def install
-    within config.local_release_path do
+    within config.local_release_app_path do
       Bundler.public_send(Bundler.respond_to?(:with_unbundled_env) ? :with_unbundled_env : :with_clean_env) do
         with bundle_app_config: config.local_base_path, rbenv_version: nil, rbenv_dir: nil do
           bundle_commands = if test :rbenv, 'version'
@@ -34,8 +34,9 @@ class Capistrano::BundleRsync::Bundler < Capistrano::BundleRsync::Base
 
   def rsync
     hosts = release_roles(:all)
+    release_app_path = config.release_app_path
     on hosts, in: :groups, limit: config.max_parallels(hosts) do
-      within release_path do
+      within release_app_path do
         execute :mkdir, '-p', '.bundle'
       end
     end
@@ -46,7 +47,7 @@ BUNDLE_FROZEN: '1'
 BUNDLE_PATH: #{shared_path.join('bundle')}
 BUNDLE_WITHOUT: #{config.bundle_without.join(':')}
 BUNDLE_DISABLE_SHARED_GEMS: '1'
-BUNDLE_BIN: #{release_path.join('bin')}
+BUNDLE_BIN: #{release_app_path.join('bin')}
     EOS
     # BUNDLE_BIN requires rbenv-binstubs plugin to make it effectively work
     bundle_config_path = "#{config.local_base_path}/bundle_config"
@@ -57,7 +58,7 @@ BUNDLE_BIN: #{release_path.join('bin')}
     Parallel.each(hosts, in_threads: config.max_parallels(hosts)) do |host|
       ssh = config.build_ssh_command(host)
       execute :rsync, "#{rsync_options} --rsh='#{ssh}' #{config.local_bundle_path}/ #{host}:#{shared_path}/bundle/"
-      execute :rsync, "#{rsync_options} --rsh='#{ssh}' #{bundle_config_path} #{host}:#{release_path}/.bundle/config"
+      execute :rsync, "#{rsync_options} --rsh='#{ssh}' #{bundle_config_path} #{host}:#{release_app_path}/.bundle/config"
     end
   end
 end
